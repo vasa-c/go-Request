b2732489e797a8f07f4205c0b91c741bf93738c5
Grigoriev Oleg <go.vasac@gmail.com>
2013-06-28 17:21:32 +0400

    CLI\Helpers\CmdArgs::convertSingleArgToCmdPart(): Convert component from args to part of command line

--- src/CLI/Helpers/CmdArgs.php
+++ src/CLI/Helpers/CmdArgs.php
@@ -21,4 +21,28 @@ class CmdArgs
         $parser = new CmdParser($cmd);
         return $parser->getArgs();
     }
+
+    /**
+     * Convert component from args to part of command line
+     *
+     * @param string $arg
+     * @return string
+     */
+    public static function convertSingleArgToCmdPart($arg)
+    {
+        $arg = \addcslashes($arg, '"');
+        if (\strpos($arg, ' ') !== false) {
+            if ($arg[0] == '-') {
+                $arg = \explode('=', $arg, 2);
+                if (\strpos($arg[0], ' ')) {
+                    $arg = '"'.$arg[0].(isset($arg[1]) ? ('='.$arg[1]) : '').'"';
+                } else {
+                    $arg = $arg[0].(isset($arg[1]) ? ('="'.$arg[1].'"') : '');
+                }
+            } else {
+                $arg = '"'.$arg.'"';
+            }
+        }
+        return $arg;
+    }
 }
--- tests/CLI/Helpers/CmdArgsTest.php
+++ tests/CLI/Helpers/CmdArgsTest.php
@@ -18,7 +18,7 @@ class CmdArgsTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @see \go\Tests\Request\CLI\Helpers\CmdParserTest
-     * @covers go\Request\CLI\Helpers::convertCmdToArgs
+     * @covers go\Request\CLI\Helpers\CmdArgs::convertCmdToArgs
      */
     public function testConvertCmdToArgs()
     {
@@ -30,5 +30,33 @@ class CmdArgsTest extends \PHPUnit_Framework_TestCase
             '--last',
         );
         $this->assertEquals($args, CmdArgs::convertCmdToArgs($cmd));
+    }
+
+    /**
+     * @covers go\Request\CLI\Helpers\CmdArgs::convertSingleArgToCmdPart
+     * @param string $arg
+     * @param string $pcmd
+     * @dataProvider providerConvertSingleArgToCmdPart
+     */
+    public function testConvertSingleArgToCmdPart($arg, $pcmd)
+    {
+        $this->assertEquals($pcmd, CmdArgs::convertSingleArgToCmdPart($arg));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerConvertSingleArgToCmdPart()
+    {
+        return array(
+            array('arg', 'arg'),
+            array('--opt', '--opt'),
+            array('ar"g', 'ar\"g'),
+            array('arg with space', '"arg with space"'),
+            array('--opt=String with space', '--opt="String with space"'),
+            array('--o pt', '"--o pt"'),
+            array('--o "pt=String with space', '"--o \"pt=String with space"'),
+            array('--opt=String with "quot"', '--opt="String with \"quot\""'),
+        );
     }
 }
