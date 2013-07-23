6b5a815f9979e5d8a985def0cc22b5bff72cd903
Grigoriev Oleg <go.vasac@gmail.com>
2013-07-23 19:02:21 +0400

    HTTP\Storage::exists(): Is var exists?

--- src/HTTP/Storage.php
+++ src/HTTP/Storage.php
@@ -8,6 +8,8 @@
 
 namespace go\Request\HTTP;
 
+use go\Request\HTTP\Helpers\Validator;
+
 class Storage
 {
     /**
@@ -35,6 +37,48 @@ class Storage
     public function getAllVars()
     {
         return $this->vars;
+    }
+
+    /**
+     * Is var exists?
+     *
+     * @param string $name
+     *        name of var
+     * @param string $type [optional]
+     *        type of var
+     * @param boolean $ex [optional]
+     *        is variable executable?
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
+    public function exists($name, $type = null, $ex = null)
+    {
+        if (!isset($this->vars[$name])) {
+            return false;
+        }
+        $value = $this->vars[$name];
+        switch ($type) {
+            case null:
+            case 'scalar':
+                return \is_scalar($value);
+            case 'float':
+                return Validator::isFloat($value);
+            case 'int':
+                return Validator::isInt($value);
+            case 'uint':
+                return Validator::isUInt($value);
+            case 'id':
+                return Validator::isId($value);
+            case 'list':
+                return Validator::isListOfScalar($value);
+            case 'dict':
+                return Validator::isDictOfScalar($value);
+            case 'array':
+                return \is_array($value);
+            case 'mixed':
+                return true;
+        }
+        throw new \InvalidArgumentException(__CLASS__.': type "'.$type.'" is invalid');
     }
 
     /**
--- tests/HTTP/StorageTest.php
+++ tests/HTTP/StorageTest.php
@@ -22,6 +22,14 @@ class StorageTest extends \PHPUnit_Framework_TestCase
     private $vars = array(
         'empty' => '',
         'scalar' => 'This is string',
+        'float' => '-123.456',
+        'int' => '-123',
+        'uint' => '0',
+        'id' => '123',
+        'list' => array('1', '2', '3'),
+        'list-tree' => array('1', array('2'), '3'),
+        'dict' => array('x' => '1', 'y' => '2'),
+        'dict-tree' => array('x' => '1', 'y' => array('2')),
     );
 
     /**
@@ -31,5 +39,96 @@ class StorageTest extends \PHPUnit_Framework_TestCase
     {
         $storage = new Storage($this->vars);
         $this->assertEquals($this->vars, $storage->getAllVars());
+    }
+
+    /**
+     * @covers go\Request\HTTP\Storage::exists
+     */
+    public function testExists()
+    {
+        $storage = new Storage($this->vars);
+
+        $this->assertTrue($storage->exists('empty'));
+        $this->assertTrue($storage->exists('scalar'));
+        $this->assertTrue($storage->exists('float'));
+        $this->assertTrue($storage->exists('id'));
+        $this->assertFalse($storage->exists('list'));
+        $this->assertFalse($storage->exists('dict'));
+        $this->assertFalse($storage->exists('unk'));
+
+        $this->assertFalse($storage->exists('empty', 'float'));
+        $this->assertFalse($storage->exists('scalar', 'float'));
+        $this->assertTrue($storage->exists('float', 'float'));
+        $this->assertTrue($storage->exists('int', 'float'));
+        $this->assertTrue($storage->exists('uint', 'float'));
+        $this->assertTrue($storage->exists('id', 'float'));
+        $this->assertFalse($storage->exists('list', 'float'));
+        $this->assertFalse($storage->exists('dict', 'float'));
+        $this->assertFalse($storage->exists('unk', 'float'));
+
+        $this->assertFalse($storage->exists('empty', 'int'));
+        $this->assertFalse($storage->exists('scalar', 'int'));
+        $this->assertFalse($storage->exists('float', 'int'));
+        $this->assertTrue($storage->exists('int', 'int'));
+        $this->assertTrue($storage->exists('uint', 'int'));
+        $this->assertTrue($storage->exists('id', 'int'));
+        $this->assertFalse($storage->exists('list', 'int'));
+        $this->assertFalse($storage->exists('dict', 'int'));
+        $this->assertFalse($storage->exists('unk', 'int'));
+
+        $this->assertFalse($storage->exists('empty', 'uint'));
+        $this->assertFalse($storage->exists('scalar', 'uint'));
+        $this->assertFalse($storage->exists('float', 'uint'));
+        $this->assertFalse($storage->exists('int', 'uint'));
+        $this->assertTrue($storage->exists('uint', 'uint'));
+        $this->assertTrue($storage->exists('id', 'uint'));
+        $this->assertFalse($storage->exists('list', 'uint'));
+        $this->assertFalse($storage->exists('dict', 'uint'));
+        $this->assertFalse($storage->exists('unk', 'uint'));
+
+        $this->assertFalse($storage->exists('empty', 'id'));
+        $this->assertFalse($storage->exists('scalar', 'id'));
+        $this->assertFalse($storage->exists('float', 'id'));
+        $this->assertFalse($storage->exists('int', 'id'));
+        $this->assertFalse($storage->exists('uint', 'id'));
+        $this->assertTrue($storage->exists('id', 'id'));
+        $this->assertFalse($storage->exists('list', 'id'));
+        $this->assertFalse($storage->exists('dict', 'id'));
+        $this->assertFalse($storage->exists('unk', 'id'));
+
+        $this->assertFalse($storage->exists('empty', 'list'));
+        $this->assertFalse($storage->exists('int', 'list'));
+        $this->assertTrue($storage->exists('list', 'list'));
+        $this->assertFalse($storage->exists('list-tree', 'list'));
+        $this->assertFalse($storage->exists('dict', 'list'));
+        $this->assertFalse($storage->exists('dict-tree', 'list'));
+        $this->assertFalse($storage->exists('unk', 'list'));
+
+        $this->assertFalse($storage->exists('empty', 'dict'));
+        $this->assertFalse($storage->exists('int', 'dict'));
+        $this->assertTrue($storage->exists('list', 'dict'));
+        $this->assertFalse($storage->exists('list-tree', 'dict'));
+        $this->assertTrue($storage->exists('dict', 'dict'));
+        $this->assertFalse($storage->exists('dict-tree', 'dict'));
+        $this->assertFalse($storage->exists('unk', 'dict'));
+
+        $this->assertFalse($storage->exists('empty', 'array'));
+        $this->assertFalse($storage->exists('int', 'array'));
+        $this->assertTrue($storage->exists('list', 'array'));
+        $this->assertTrue($storage->exists('list-tree', 'array'));
+        $this->assertTrue($storage->exists('dict', 'array'));
+        $this->assertTrue($storage->exists('dict-tree', 'array'));
+        $this->assertFalse($storage->exists('unk', 'array'));
+
+        $this->assertTrue($storage->exists('empty', 'mixed'));
+        $this->assertTrue($storage->exists('int', 'mixed'));
+        $this->assertTrue($storage->exists('list', 'mixed'));
+        $this->assertTrue($storage->exists('list-tree', 'mixed'));
+        $this->assertTrue($storage->exists('dict', 'mixed'));
+        $this->assertTrue($storage->exists('dict-tree', 'mixed'));
+        $this->assertFalse($storage->exists('unk', 'mixed'));
+
+        $this->setExpectedException('InvalidArgumentException');
+        $storage->exists('empty', 'invalid');
     }
 }
