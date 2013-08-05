096c1589c00839262b75f15ede5cb7ee297e4485
Grigoriev Oleg <go.vasac@gmail.com>
2013-08-05 15:29:41 +0400

    Phar: stub for phar archive

---
+++ src/phar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Stub for phar archive
+ *
+ * @example include('phar:///path/to/go/Request.phar')
+ *
+ * @package go\Request
+ * @author Grigoriev Oleg aka vasa_c <go.vasac@gmail.com>
+ */
+
+namespace go\Request;
+
+require_once(__DIR__.'/Autoloader.php');
+Autoloader::register();
